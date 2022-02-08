196db7a2c9e0ed849dbc8d3d8a2f5ba095e32b97
Jean-Luc Duvivier <jean-luc.orness@duvivier.bzh>
2022-02-08 16:31:22 +0100

    Try to set short name

--- lidy-js/src/listener/service_template.js
+++ lidy-js/src/listener/service_template.js
@@ -6,5 +6,25 @@ export default {
         cst.tosca_definitions_version = val.tosca_definitions_version?.value
         cst.description = val.description?.value.tosca
         cst.metadata = val.metadata?.value.tosca
+
+        // for (const idx in cst.node_types) {
+        //     parts = idx.substring(str.lastIndexOf('/')+1).split('.')
+        //     let idxs = []
+        //     for(i=0, subIdx="", sep=""; 
+        //         parts.at(i)!=null;
+        //         i--, subIdx=parts.at(i)+ sep + subIdx, sep=".") { 
+        //         // si Filter
+                
+        //             // idxs.push(subIdx)
+        //     }
+
+        //     idxs.forEach(part => {
+        //         if (node.filter(x => x.endsWith(part).length == 1)) {
+        //             node.push(part)
+        //         }
+        //     });
+                
+            // }
+        // }
     }
 }
