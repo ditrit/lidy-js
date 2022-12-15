0f9018f72c8ade739018cc7a2597fef63d2ea8e4
Vincent Moittie <moittie.vincent@gmail.com>
2022-12-15 11:43:44 +0100

    Make this library works with web application

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { preprocess } from "./parser/node_parse.js"
 import { parse } from "./parser/parse.js"
 import { LidyError } from './parser/errors.js'
 
 export {
-    preprocess, parse, LidyError
+    parse, LidyError
 }
