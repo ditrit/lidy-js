3174b4da96229e62d590d73c95160c0b9cdb9410
Thibaud Labat <thibaud.labat@hotmail.fr>
2023-08-29 10:18:15 +0200

    Option: throw on error

--- parser/ruleparser.js
+++ parser/ruleparser.js
@@ -1,9 +1,17 @@
 import { parse_rule } from "./parse.js"
-
 export class RuleParser {
   static scalartypes  = ['string', 'int', 'float', 'null', 'boolean', 'binary', 'timestamp']
   static keywords     = [ '_map', '_mapOf', '_mapFacultative','_list', '_listOf', '_listFacultative', '_oneOf', '_regex', '_nb', '_min', '_max', '_in'] // merge?
+  static throwOnError = false;
 
+  /**
+   *
+   * @param {Ctx} ctx
+   * @param {string} rule_name
+   * @param rule
+   * @param current
+   * @returns {StringNode|BinaryNode|TimestampNode|IntNode|FloatNode|BooleanNode|NullNode|MapNode|ListNode|null}
+   */
   static parse(ctx, rule_name, rule, current) {
 
     if (current == null) {
@@ -25,6 +33,16 @@ export class RuleParser {
 
     // Parse rule
     let  parsedRule = parse_rule(ctx, null, rule, current)
+
+    if(this.throwOnError && parsedRule===null)
+    {
+      if(ctx.errors.length !== 0)
+      {
+        throw ctx.errors[ctx.errors.length-1];
+      }
+      else
+        throw new Error("Unknown error encountered while parsing.");
+    }
 
     // Call exit listener if it exists
     let fexit = "exit_" + rule_name
